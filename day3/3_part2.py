eaf65c50c22e209bf626ed662e6d9544f84d5d0a
sjboy7 <121656886+sjboy7@users.noreply.github.com>
2024-12-05 08:07:17 +1100

    day3 added cProfile

--- day3/3_part2.py
+++ day3/3_part2.py
@@ -1,25 +1,31 @@
 import re
+import time
+import cProfile
 
-regex_mul = r'^mul\(\d{1,3},\d{1,3}\)'
-regex_vals = r'(\d{1,3})'
-regex_do = r'^do\(\)'
-regex_dont = r'^don\'t\(\)'
+def main():
 
-do=True
+    regex_mul = r'^mul\(\d{1,3},\d{1,3}\)'
+    regex_vals = r'(\d{1,3})'
+    regex_do = r'^do\(\)'
+    regex_dont = r'^don\'t\(\)'
 
-with open('3_input', 'r') as file:
-    data = file.readlines()
+    do=True
+
+    with open('3_input', 'r') as file:
+        data = file.readlines()
 
 
-sum=0
-for line in data:
-    for i in range(len(line)):
-        if re.search(regex_do,line[i:]):
-            do=True
-        if re.search(regex_dont,line[i:]):
-            do=False
-        mul_operation=re.search(regex_mul,line[i:])
-        if(mul_operation) and do:
-            mul_vals=list(map(int,re.findall(regex_vals,mul_operation.group())))
-            sum+=mul_vals[0]*mul_vals[1]
-print(sum)
+    sum=0
+    for line in data:
+        for i in range(len(line)):
+            if re.search(regex_do,line[i:]):
+                do=True
+            if re.search(regex_dont,line[i:]):
+                do=False
+            mul_operation=re.search(regex_mul,line[i:])
+            if(mul_operation) and do:
+                mul_vals=list(map(int,re.findall(regex_vals,mul_operation.group())))
+                sum+=mul_vals[0]*mul_vals[1]
+    print(sum)
+
+cProfile.run('main()')
