91ace37dd10bef21dfa4264481ed53c5429dbdc4
sjboy7 <121656886+sjboy7@users.noreply.github.com>
2024-12-05 07:54:00 +1100

    day3 update

--- day3/3_part2.py
+++ day3/3_part2.py
@@ -4,11 +4,16 @@ regex_mul = r'mul\(\d{1,3},\d{1,3}\)'
 regex_num = r'(\d{1,3})'
 regex_do = r'(do\(\))'
 regex_dont = r'(don\'t\(\))'
-regex_valid_start = r'^(.+)don\'\(\)'
-regex_valid_middle = r'do\(\)(.+)don\'t\(\)'
-regex_valid_end = r'do\(\)$(.+)'
-regex_valid_all = r'^(.+)don\'t\(\)'
+# regex_valid_start = r'(.+?)^don\'t\(\)'
+regex_valid_start = r"^(.*?)don\'t\(\)"
+regex_valid_middle = r"do\(\)(.+?)don't\(\)"
+# regex_valid_end = r'do\(\)(.+?)'
+regex_valid_end = r'(?<=do\(\)).*$'
+
+# regex_valid_end = r'(^(don\'t\(\))(.+?)$'
+# regex_valid_all = r'^(.+)don\'t\(\)'
 # regex_valid_all = r'(.+)do\(\)$(.+)don\'t\(\)(.+)'
+regex_valid_all = r''
 
 do=True
 
@@ -25,9 +30,71 @@ with open('3_input', 'r') as file:
 # lines_extracted_iter = [re.finditer(regex_mul, data[i]) for i in range(len(data))]
 # do_extracted_iter = [re.finditer(regex_do, data[i]) for i in range(len(data))]
 # dont_extracted_iter = [re.finditer(regex_dont, data[i]) for i in range(len(data))]
-valid_extracted = [re.findall(regex_valid_all, data[i]) for i in range(len(data))]
+# valid_extracted = [re.findall(regex_valid_all, data[i]) for i in range(len(data))]
+valid_extracted=[]
+for line_no, line in enumerate(data):
+    valid_start= list(re.findall(regex_valid_start, line))
+    if len(valid_start): # (there is a don't)
+        valid_extracted.append(valid_start)
+        valid_end =  list(re.findall(regex_valid_end, line))
+        if len(valid_end): # (there is a do)
+            valid_middle = list(re.findall(regex_valid_middle, line))
+            if (re.search(regex_do, valid_start[0])): # (there is a do in the first extract)
+                valid_middle = valid_middle[1:] # drop the first extract in the middle
+            if(re.search(regex_dont, valid_end[0])): # (there is a don't in the last extract)
+                valid_end=None # drop the end extract
+            if (valid_middle): valid_extracted.append(valid_middle)
+            if (valid_end): valid_extracted.append(valid_end)
 
-print(list(valid_extracted)[0])
+    else: # there aren't any don't, the whole line is valid
+        valid_extracted.append(line)
+
+    # print(f"{line_no}\t{valid_extracted}")
+    # print()
+    # print(valid_end)
+    # exit()
+    # print(valid_start[0])
+    # exit()
+    
+    # break
+    
+# so now valid_extracted is just the valid sections of the input
+# exit()
+# print(len(valid_extracted))
+# print(valid_extracted[0])
+# print(valid_extracted[1])
+# print(valid_extracted[2])
+
+# exit()
+muls_extracted=[]
+
+# print(len(valid_extracted))
+# print(valid_extracted[0])
+# exit()
+for line in valid_extracted:
+    # print(line)
+    # print()
+    for extract in line:
+        mul_extracted=re.findall(regex_mul, extract)
+        if (mul_extracted):
+            muls_extracted.append(mul_extracted)
+    
+# exit()
+print(muls_extracted)
+# exit()
+# print(muls_extracted[0])
+sum=0
+for muls in muls_extracted:
+    for mul in muls:
+        print(mul)
+        vals_extracted=list(map(int, re.findall(regex_num,mul)))
+        sum+=vals_extracted[0]*vals_extracted[1]
+
+print(sum)
+
+
+
+exit()
 
 # print(list(lines_extracted_iter))
 # mul_array=[]
