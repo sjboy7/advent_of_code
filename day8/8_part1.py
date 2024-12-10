84d9ac30975223bce8b8d1053d78fef4c6f94e3d
sjboy7 <121656886+sjboy7@users.noreply.github.com>
2024-12-11 07:15:32 +1100

    day8 update

--- day8/8_part1.py
+++ day8/8_part1.py
@@ -1,21 +1,46 @@
 import re
 import numpy as np
 
-with open('8_input_example') as f:
+
+def append_node(node):
+    if node[0]>=0 and node[0]<grid_limit_x and node[1]>=0 and node[1]<grid_limit_y:
+        nodes.add((node[0],node[1]))
+        # for existing_node in nodes:
+        #     if (node==existing_node).all():
+        #         break
+        # else:
+        #     nodes.append(node)
+            
+
+# with open('8_input_example') as f:
+with open('8_input') as f:
     data=f.readlines()
 
 antenna_dict = {}
 
 for row, line in enumerate(data):
     antennas=re.finditer(r'(\d)|[A-Z]|[a-z]',line.strip())
-    # antennas=re.finditer(r'(\d)',line.strip())
-    # antennas=re.finditer(r'[A-Z]',line.strip())
-    # print(list(antennas.group()))
-    # print(list(antennas).start())
-    # if (antennas):
     for antenna in antennas:
-        antenna_dict[antenna.group()]=np.array([antenna.start(),row*-1])
-        # print(antenna.group())
+        antenna_name=antenna.group()
+        antenna_location=np.array([antenna.start(),row])
+        if antenna_name in antenna_dict:
+            antenna_dict[antenna_name].append(antenna_location)
+        else:
+            antenna_dict[antenna_name]=[antenna_location]
 
-print(antenna_dict)
-exit()
+grid_limit_x=len(data)
+grid_limit_y=len(data[0].strip())
+
+
+nodes=set()
+for antenna_name in antenna_dict:
+    for i in range(len(antenna_dict[antenna_name])):
+
+        antenna1_pos=antenna_dict[antenna_name][i]
+        for j in range(i):
+            antenna2_pos=antenna_dict[antenna_name][j]
+            separation=antenna1_pos-antenna2_pos
+            append_node(antenna1_pos+separation)
+            append_node(antenna2_pos-separation)
+
+print(len(nodes))
