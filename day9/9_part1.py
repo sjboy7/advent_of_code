b34668640d040a3a12374c2ad31bc68331af6546
Simon Boyd <sjboy7@gmail.com>
2024-12-13 19:31:22 +1100

    day9 update

--- day9/9_part1.py
+++ day9/9_part1.py
@@ -1,5 +1,14 @@
 with open('9_input_example') as f:
     data = f.readlines()[0].strip()
 
-for i in data:
-    print(i)
+blocks = ""
+for index, num in enumerate(data):
+    
+    if (index & 1):
+        blocks = blocks + int(num)*'.'
+
+    else:
+        blocks = blocks + int(num)*str(index>>1)
+
+
+print(blocks)
